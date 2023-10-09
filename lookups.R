49bcf875bebe58d4ceb1e1b940541d21a92486d2
lmkirvan <kirvan.lewis@gmail.com>
2023-10-09 09:38:08 -0400

    some progress but a long way to go still.

--- lookups.R
+++ lookups.R
@@ -28,3 +28,19 @@ disp <- bind_rows(
       , "appeal deny")
   )
 )
+
+
+presidents <- c("George Washington","John Adams","Thomas Jefferson"
+                ,"James Madison","James Monroe","John Quincy Adams"
+                ,"Andrew Jackson","Martin Van Buren","William Henry Harrison"
+                ,"John Tyler","James K. Polk","Zachary Taylor","Millard Fillmore"
+                ,"Franklin Pierce","James Buchanan","Abraham Lincoln","Andrew Johnson"
+                ,"Ulysses S. Grant","Rutherford B. Hayes","James A. Garfield"
+                ,"Chester Arthur","Grover Cleveland^","Benjamin Harrison"
+                ,"Grover Cleveland^","William McKinley","Theodore Roosevelt"
+                ,"William Howard Taft","Woodrow Wilson","Warren G. Harding"
+                ,"Calvin Coolidge","Herbert Hoover","Franklin D. Roosevelt"
+                ,"Harry S. Truman","Dwight D. Eisenhower","John F. Kennedy"
+                ,"Lyndon B. Johnson","Richard M. Nixon","Gerald Ford","Jimmy Carter"
+                ,"Ronald Reagan","George H. W. Bush","Bill Clinton","George W. Bush"
+                ,"Barack Obama","Donald Trump","Joe Biden")
